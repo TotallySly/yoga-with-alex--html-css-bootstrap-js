43a61337a129b588553db0f2d43ebe8ef70ca1b1
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2022-06-21 14:00:28 +0200

    Fixed toggle for hamburger navigation with JS toggle

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const button = document.querySelector('.navbar-toggler')
 
+button.addEventListener('click', () => {
+    const hamburgerOpen = document.querySelector('.hamburger-open')
+    const hamburgerClose = document.querySelector('.hamburger-close')
+
+    hamburgerOpen.classList.toggle('hide-element')
+    hamburgerClose.classList.toggle('hide-element')
+})
