3753cce444eaa2b7bc90c7b54994754805841049
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2022-07-06 15:36:53 +0200

    Add notes to JS script

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+// Global Variables
+
 const button = document.querySelector('.navbar-toggler')
 
+/**
+ * The Bootstrap v.5.2 hamburger did not work properly. I have manually created a 'click' event listener so the
+ * hamburger menu now works.
+ */
 button.addEventListener('click', () => {
     const hamburgerOpen = document.querySelector('.hamburger-open')
     const hamburgerClose = document.querySelector('.hamburger-close')
