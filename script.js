e7a8985f7ffe0eeba706cee649a56c48ef26dd04
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2022-07-07 12:46:42 +0200

    Create About me boiler plate for how I would like content to enter for tablets and higher

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Global Variables
 
 const button = document.querySelector('.navbar-toggler')
+const boxes = document.querySelectorAll('.box')
 
 /**
  * The Bootstrap v.5.2 hamburger did not work properly. I have manually created a 'click' event listener so the
@@ -13,3 +14,21 @@ button.addEventListener('click', () => {
     hamburgerOpen.classList.toggle('hide-element')
     hamburgerClose.classList.toggle('hide-element')
 })
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = (window.innerHeight / 5) * 4
+
+    boxes.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if (boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
